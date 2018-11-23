ab75a78d05a468495435312670e71768f43ea3ab
dbuhtiyarov <devops@test.test>
2018-11-23 13:17:04 +0000

    db added plus users

--- atlassian/db/terraform/db.tf
+++ atlassian/db/terraform/db.tf
@@ -1,4 +1,7 @@
 variable "project_name" {}
+variable "user" {}
+variable "password" {}
+
 
 provider "google" {
   credentials = "${file("~/.gcp/account.json")}"
@@ -12,6 +15,15 @@ terraform {
   }
 }
 
+data "null_data_source" "auth_netw_postgres_allowed_2" {
+  count = 1
+
+  inputs = {
+    name  = "onprem-${count.index + 1}"
+    value = "${element(list("0.0.0.0/0"), count.index)}"
+  }
+}
+
 resource "google_sql_database_instance" "master" {
   name = "master-instance"
   database_version = "POSTGRES_9_6"
@@ -19,6 +31,23 @@ resource "google_sql_database_instance" "master" {
 
   settings {
     tier = "db-f1-micro"
+
+  ip_configuration {
+      authorized_networks = [
+        "${data.null_data_source.auth_netw_postgres_allowed_2.*.outputs}",
+      ]
+    }
+
   }
 }
 
+resource "google_sql_database" "users" {
+  name      = "jira-dev"
+  instance  = "${google_sql_database_instance.master.name}"
+}
+
+resource "google_sql_user" "users" {
+  name     = "${var.user}"
+  instance = "${google_sql_database_instance.master.name}"
+  password = "${var.password}"
+}
